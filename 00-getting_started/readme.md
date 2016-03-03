4759c56160965e49a4f4a9d2be0de6f3cb012442
samskito <samtheskito@gmail.com>
2016-03-03 10:29:35 +0100

    Logs in both client and server side. Bidirectionnal communication.

--- 00-getting_started/readme.md
+++ 00-getting_started/readme.md
@@ -99,5 +99,13 @@ Socket.io is a JavaScript library for realtime web apps
 
 ## Exemple based on this [Youtube Video](https://www.youtube.com/watch?v=HtzA1sCr5FQ&list=PLw5h0DiJ-9PC0Wo1NWrNHgKE-mFc_9ftq): Build Podcast 063 Socket IO
 
+** Install WebSocket module via npm
+* `npm install --save ws`
 
+** Start the node server
+* `node server.js`
+* This will show the messages sent by the server in the client's console and the client's messages in the server's terminal
+
+**clearInterval(id);
+* Stop a timer that was previously created with setInterval.
 
--- 00-getting_started/server.js
+++ 00-getting_started/server.js
@@ -1,1 +1,35 @@
-server.js
+var WebSocketServer = require('ws').Server; // from npm install
+var http = require('http');
+var port = process.env.PORT || 5000;
+var server = http.createServer();
+
+server.listen(port)
+
+// Create the websocket
+var wss = new WebSocketServer({
+	server: server
+});
+
+console.log('websocket server created');
+
+// When the connection opens
+wss.on('connection', function(ws) {
+	console.log('websocket connection open');
+
+	// On message from the client
+	ws.on('message', function(message) {
+		console.log('message recieved: ' + message);
+	});
+
+	// When the connection closes
+	ws.on('close', function() {
+		console.log('websocket connection closed');
+		clearInterval(id); // Stop a timer that was previously created with setInterval.
+	});
+
+	// Object created when connection starts
+	var id = setInterval(function() {
+		var message = 'ping from server: ' + new Date()
+		ws.send(message, function(){});
+	}, 1000);
+});
